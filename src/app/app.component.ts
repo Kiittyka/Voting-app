39bf70b8448079096b8701212bc860f2fc69ef82
Kiittyka <krithikaudupa@gmail.com>
2020-08-26 18:21:45 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'voting-app';
+  votes = new Array(4).fill(0);
+
+  updateVote(index) {
+    this.votes[index] += 1;
+  }
 }
